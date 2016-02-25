03e0f97989653cbb6153c47dabc1176873d794f6
Gr4cchus <ahivent@gmail.com>
2016-02-25 05:02:15 -0600

    1 Ch4 left to do

--- Ch4/MisleadingPercentages.py
+++ Ch4/MisleadingPercentages.py
@@ -1,7 +1,27 @@
 # Misleading Percentages
-initialInvestment = 10000
-totalYield = initialInvestment * 1.18
-print(totalYield)
-for biannually in range(2):
-    for month in range(6):
+currentInvestmentTotal = 10000
+positivePercentageYield = 1.18
+negativePercentageYield = .84
 
+firstBiannualDifference = 0.0
+secondBiannualDifference = 0.0
+
+for firstMonthSet in range(6):
+    currentInvestmentTotal *= negativePercentageYield
+    firstBiannualDifference = 10000 - currentInvestmentTotal
+    print(format(currentInvestmentTotal, ',.2f'))
+    firstInvestmentTotal = currentInvestmentTotal
+
+print()
+
+for secondMonthSet in range(6):
+    currentInvestmentTotal *= positivePercentageYield
+    secondBiannualDifference = 10000 - currentInvestmentTotal
+    print(format(currentInvestmentTotal, ',.2f'))
+    secondInvestmentTotal = currentInvestmentTotal - firstInvestmentTotal
+
+print("The first half of the Biannual year saw a loss of", format(firstBiannualDifference, ',.2f'),
+      "from initial investment.")
+print("The second half of the Biannual year saw a loss of", format(secondBiannualDifference, ',.2f'),
+      "from initial investment,\n but a gain of", format(secondInvestmentTotal, ',.2f'),
+      "from the previous six months.")
--- Ch4/SupplyAndDemand.py
+++ Ch4/SupplyAndDemand.py
@@ -1,1 +1,11 @@
 # Supply and Demand
+soybeanQuantity = 80.00
+startingPrice = 12.00
+
+print("YEAR", "QUANTITY", "PRICE", sep="\t\t")
+print("2014", soybeanQuantity, startingPrice, sep="\t\t")
+
+for year in range(2015, 2019):
+    startingPrice = 20 - .1 * soybeanQuantity
+    soybeanQuantity = 5 * startingPrice - 10
+    print(year, soybeanQuantity, startingPrice, sep="\t\t")
