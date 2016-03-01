fe777b835de32039cfb3a8da39ebe28e9666594c
ltop <ahivent@gmail.com>
2016-02-29 18:00:29 -0600

    Supply and demand finished from laptop. Will Misleading produce errors from other source commit?

--- Ch4/SupplyAndDemand.py
+++ Ch4/SupplyAndDemand.py
@@ -1,1 +1,10 @@
 # Supply and Demand
+quantity = 80
+price = 18
+
+print("Year", "Quantity", "Price", sep="\t\t")
+
+for year in range(2014,2019):
+    quantity = 5 * price - 10
+    price = 20 - .1 * quantity
+    print(year, quantity, price, sep="\t\t")
