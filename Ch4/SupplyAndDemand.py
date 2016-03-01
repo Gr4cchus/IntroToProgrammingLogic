e383e234e5d50713a94b55773452bb9cb4b521d4
Gr4cchus <ahivent@gmail.com>
2016-02-29 18:08:41 -0600

    Merged Misleading previously into this push? Updated Supply and Demand Formatting.

--- Ch4/SupplyAndDemand.py
+++ Ch4/SupplyAndDemand.py
@@ -2,9 +2,9 @@
 quantity = 80
 price = 18
 
-print("Year", "Quantity", "Price", sep="\t\t")
+print("Year", "Quantity", "Price", sep="\t")
 
 for year in range(2014,2019):
     quantity = 5 * price - 10
     price = 20 - .1 * quantity
-    print(year, quantity, price, sep="\t\t")
+    print(year, format(quantity, '.2f'), format(price, '.2f'), sep="\t")
